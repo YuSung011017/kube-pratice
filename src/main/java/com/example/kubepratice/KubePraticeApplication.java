1ee73418985fd7ac753ec4e2a859e329c1ff8563
YuSung011017 <salamander011017@gmail.com>
2025-02-21 15:50:23 +0900

    first commit

---
+++ src/main/java/com/example/kubepratice/KubePraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubepratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubePraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubePraticeApplication.class, args);
+    }
+
+}
